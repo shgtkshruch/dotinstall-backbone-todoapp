d759a0884a7afeabcc9ddf5a9528e19b43071e61
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-30 13:56:34 +0900

    Create model.

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 (function() {
   'use strict';
 
+  // Model
+
+  var Task = Backbone.Model.extend({
+    defaults: {
+      title: 'do something',
+      completed: false
+    }
+  });
+
+  var Tasks = Backbone.Collection.extend({
+    model: Task
+  });
+
+  // View
+
+  var TaskView = Backbone.View.extend({
+
+  });
+
+  var TaksksView = Backbone.View.extend({
+
+  });
+
+  var tasks = new Tasks([
+      { title: 'task1' , completed: true},
+      { title: 'task2' },
+      { title: 'task3' }
+  ]);
 
 }());
