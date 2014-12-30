3c6074ca5fee82247659ae579212487fb9e4b7c0
shgtkshruch <shgtk.shruch@gmail.com>
2014-12-30 15:11:48 +0900

    First pages commit.

---
+++ app.js
@@ -0,0 +1,125 @@
+(function() {
+  'use strict';
+
+  // Model
+
+  var Task = Backbone.Model.extend({
+    defaults: {
+      title: 'do something',
+      completed: false
+    },
+    validate: function (attrs) {
+      if (_.isEmpty(attrs.title)) {
+        return 'Title must not be empty';
+      }
+    },
+    initialize: function () {
+      this.on('invalid', function (model, err) {
+        $('#err').html(err);
+      });
+    }
+  });
+
+  var Tasks = Backbone.Collection.extend({ model: Task });
+
+  // View
+
+  var template = _.template($('#task-template').html());
+
+  var TaskView = Backbone.View.extend({
+    tagName: 'li',
+    initialize: function () {
+      this.model.on('destroy', this.remove, this);
+      this.model.on('change', this.render, this);
+    },
+    events: {
+      'click .delete': 'destroy',
+      'click .toggle': 'toggle'
+    },
+    destroy: function (e) {
+      if (confirm('Are you sure?')) {
+        this.model.destroy();
+      }
+    },
+    remove: function () {
+      this.$el.remove();
+    },
+    toggle: function (e) {
+      this.model.set('completed', !this.model.get('completed'));
+    },
+    template: template,
+    render: function () {
+      var template = this.template(this.model.toJSON());
+      this.$el.html(template);
+
+      return this;
+    }
+  });
+
+  var TaksksView = Backbone.View.extend({
+    tagName: 'ul',
+    initialize: function () {
+      this.collection.on('add', this.addNew, this);
+      this.collection.on('change', this.updateCount, this);
+      this.collection.on('destroy', this.updateCount, this);
+    },
+    addNew: function (task) {
+      var taskView = new TaskView({ model: task });
+      this.$el.append(taskView.render().el);
+
+      $('#title').val('').focus();
+
+      this.updateCount();
+    },
+    updateCount: function () {
+      var uncompletedTasks = this.collection.filter(function (task) {
+        return !task.get('completed');
+      });
+
+      $('#count').html(uncompletedTasks.length);
+    },
+    render: function () {
+      this.collection.each(function (task) {
+        var taskView = new TaskView({ model: task });
+
+        this.$el.append(taskView.render().el);
+      }.bind(this));
+
+      this.updateCount();
+
+      return this;
+    }
+  });
+
+  // form
+
+  var AddTaskView = Backbone.View.extend({
+    el: '#addTask',
+    events: {
+      'submit': 'submit'
+    },
+    submit: function (e) {
+      e.preventDefault();
+
+      var task = new Task();
+
+      if (task.set({ title: $('#title').val(), completed: false }, { validate: true })) {
+        this.collection.add(task);
+        $('#err').empty();
+      }
+    }
+  });
+
+  var tasks = new Tasks([
+      { title: 'task1' , completed: true},
+      { title: 'task2' },
+      { title: 'task3' }
+  ]);
+
+  var tasksView = new TaksksView({ collection: tasks });
+  var addTaskView = new AddTaskView({ collection: tasks });
+
+  $('#tasks').append(tasksView.render().el);
+
+}());
+
